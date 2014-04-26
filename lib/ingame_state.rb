c472544f60871e21c1ac48c4ff02c585771d110c
Daniel Smith <jellymann@gmail.com>
2014-04-26 17:56:05 +0200

    Added white holes

--- lib/ingame_state.rb
+++ lib/ingame_state.rb
@@ -28,7 +28,7 @@ class IngameState < EngineState
         e[:acceleration][:y] -= e[:player][:a]
       end
     end
-    .system(:draw, :sprite_draw, [:position, :sprite, :rotation]) do |e|
+    .system(:draw, :sprite_draw_rotated, [:position, :sprite, :rotation]) do |e|
       @window::translate(*world2screen(0,0)) do
         x = e[:position][:x]
         y = e[:position][:y]
@@ -36,6 +36,26 @@ class IngameState < EngineState
         dx = e[:sprite][:anchor][:x]
         dy = e[:sprite][:anchor][:y]
         img.draw_rot x, y, 0, e[:rotation][:theta], dx, dy
+      end
+    end
+    .system(:draw, :sprite_draw, [:position, :sprite, :norotate]) do |e|
+      @window::translate(*world2screen(0,0)) do
+        x = e[:position][:x]
+        y = e[:position][:y]
+        img = e[:sprite][:image]
+        dx = e[:sprite][:anchor][:x]*img.width
+        dy = e[:sprite][:anchor][:y]*img.height
+        img.draw x-dx, y-dy, 0
+      end
+    end
+    .system(:draw, :white_hole_draw, [:position, :sprite, :white_hole]) do |e|
+      @window::translate(*world2screen(0,0)) do
+        x = e[:position][:x]
+        y = e[:position][:y]
+        img = e[:sprite][:image]
+        dx = e[:sprite][:anchor][:x]*img.width*e[:white_hole][:size]
+        dy = e[:sprite][:anchor][:y]*img.height*e[:white_hole][:size]
+        img.draw x-dx, y-dy, 0, e[:white_hole][:size], e[:white_hole][:size]
       end
     end
     .system(:update, :acceleration, [:velocity, :acceleration]) do |dt, t, e|
@@ -60,6 +80,12 @@ class IngameState < EngineState
       e[:position][:y] = my
       e
     end
+    .system(:update, :pulse_hole, [:white_hole, :pulsate]) do |dt, t, e|
+      if t % e[:pulsate][:period] < @window.update_interval
+        e[:white_hole][:size] = e[:pulsate][:base_size]*Gosu::random(e[:pulsate][:min],e[:pulsate][:max])
+      end
+      e
+    end
     .add_entity({
       :player => {:a => 30},
       :follow_mouse => {},
@@ -74,8 +100,11 @@ class IngameState < EngineState
       :cursor => true,
       :sprite => ECS::make_sprite(Gosu::Image.new @window, "cursor.png"),
       :position => {:x => 0, :y => 0},
-      :rotation => {:theta => 0}
+      :norotate => true
     })
+    .add_entity(gen_white_hole(-100,-100))
+    .add_entity(gen_white_hole(200,-100))
+    .add_entity(gen_white_hole(50,200))
   end
 
   def screen2world x, y
@@ -84,5 +113,15 @@ class IngameState < EngineState
 
   def world2screen x, y
     [x+@window.width/2, y+@window.height/2]
+  end
+
+  def gen_white_hole x, y
+    {
+      :white_hole => {:size => 0},
+      :pulsate => {:min => Gosu::random(0.7,0.9), :max => Gosu::random(1.1,1.3), :period => 0.2,
+        :base_size => Gosu::random(0.6,1.4)},
+      :position => {:x => x, :y => y},
+      :sprite => ECS::make_sprite(Gosu::Image.new @window, "white_hole.png")
+    }
   end
 end

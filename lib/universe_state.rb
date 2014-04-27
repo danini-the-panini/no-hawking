d722f07b9ec572d6c65a343cdfa0c4a0dfe7c2ff
Daniel Smith <jellymann@gmail.com>
2014-04-27 16:23:12 +0200

    Added hawking bar to universe view

--- lib/universe_state.rb
+++ lib/universe_state.rb
@@ -4,12 +4,20 @@ class UniverseState < IngameState
   def initialize window, universe
     super window
 
+    @initial_hawking_cap = 1.0
+
     @engine
     .input_system(:down, :escape_universe, []) do |id|
       return_to_multiverse if id == Gosu::KbSpace
     end
-    .system(:update, :probe_life, [:probe]) do |dt, t, e|
-      if e[:probe][:health] <= 0.0
+    .system(:update, :hawking_bar, [:hawking_bar]) do |dt, t, e|
+      @engine.each_entity([:player, :hawking, :probe]) do |pl|
+        e[:scale][:x] = pl[:hawking]/pl[:probe][:hawking_cap]
+      end
+      e
+    end
+    .system(:update, :probe_life, [:player, :health]) do |dt, t, e|
+      if e[:health] <= 0.0
         return_to_multiverse # TODO: animate explosion or something first
         remove e
       else
@@ -25,10 +33,23 @@ class UniverseState < IngameState
     .add_entity(
       gen_player.merge({
         :sprite => make_sprite(Gosu::Image.new @window, "spr_player.png"),
-        :probe => {:hawking => 0, :hawking_cap => @initial_hawking_cap, :xp => 0,
-          :health => 1.0, :health_cap => 1.0, :armour_mult => 1.0,
-          :speed_mult => 1.0}
+        :hawking => 0.0,
+        :health => 1.0,
+        :probe => {:hawking_cap => @initial_hawking_cap, :xp => 0,
+          :health_cap => 1.0, :armour_mult => 1.0, :speed_mult => 1.0}
       }))
+    .add_entity({
+      :hud => true,
+      :hawking_bar => true,
+      :position => {:x => 10, :y => @window.height-10},
+      :sprite => make_sprite((Gosu::Image.new @window, "hawking_bar.png"),{:x => 0.0, :y => 1.0}),
+      :scale => {:x => 0.0, :y => 1.0}
+    })
+    .add_entity({
+      :hud => true,
+      :position => {:x => 10, :y => @window.height-10},
+      :sprite => make_sprite((Gosu::Image.new @window, "hawking_bar_border.png"),{:x => 0.0, :y => 1.0})
+    })
     
   end
 

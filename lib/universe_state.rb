b64197153158501b075a56205822d00c66760eed
Daniel Smith <jellymann@gmail.com>
2014-04-29 02:16:18 +0200

    Parameterized guns

--- lib/universe_state.rb
+++ lib/universe_state.rb
@@ -12,8 +12,13 @@ class UniverseState < IngameState
     @collect_strength = 10.0
     @collect_threshold = 10.0
 
-    @bullet_speed = 150.0
-    @gun_damage = 0.1
+    @player_fire_rate = 5.0
+    @player_bullet_speed = 300.0
+    @player_damage = 0.1
+
+    @enemy_fire_rate = 2.0
+    @enemy_bullet_speed = 500.0
+    @enemy_damage = 0.1
 
     @spr_bullet = Gosu::Image.new @window, "spr_bullet.png"
 
@@ -60,12 +65,14 @@ class UniverseState < IngameState
       e
     end
     .system(:update, :hawking_bar, [:hawking_bar]) do |dt, t, e|
-      pl = @engine.get_entity @player_id
-      e[:scale][:x] = pl[:hawking]/pl[:probe][:hawking_cap]
+      unless (pl = @engine.get_entity @player_id).nil?
+        e[:scale][:x] = pl[:hawking]/pl[:probe][:hawking_cap]
+      end
       e
     end
-    .system(:update, :weapon, [:player, :probe]) do |dt, t, e|
-      if @engine.down? Gosu::MsLeft
+    .system(:update, :weapon, [:player, :probe, :weapon]) do |dt, t, e|
+      if @engine.down?(Gosu::MsLeft) && (t-e[:weapon][:last_fire] > 1.0/e[:weapon][:fire_rate])
+        e[:weapon][:last_fire] = t
         mx, my = screen2world(@window.mouse_x, @window.mouse_y)
         rad = Math::atan2(my-e[:position][:y], mx-e[:position][:x])
         theta = (rad*180.0)/Math::PI
@@ -73,9 +80,9 @@ class UniverseState < IngameState
           :position => e[:position].dup,
           :sprite => make_sprite(Gosu::Image.new @window, @spr_bullet),
           :rotation => {:theta => theta},
-          :velocity => {x: @bullet_speed*Math.cos(rad) + e[:velocity][:x],
-            :y => @bullet_speed*Math::sin(rad) + e[:velocity][:y] },
-          :bullet => {:damage => @gun_damage, :owner => e[:id]},
+          :velocity => {x: e[:weapon][:speed]*Math.cos(rad) + e[:velocity][:x],
+            :y => e[:weapon][:speed]*Math::sin(rad) + e[:velocity][:y] },
+          :bullet => {:damage => e[:weapon][:damage], :owner => e[:id]},
           :life => 4, :lifetime => 4
         }))
       end
@@ -101,16 +108,17 @@ class UniverseState < IngameState
       e
     end
     .system(:update, :ai_target, [:enemy, :position]) do |dt, t, e|
-      pl = @engine.get_entity @player_id
-      dist_to_player = dist_sq(e[:position][:x],e[:position][:y],pl[:position][:x],pl[:position][:y])
-      if dist_to_player < sq(e[:enemy][:alert_radius])
-        e[:enemy][:target] = {:x => pl[:position][:x] + pl[:velocity][:x]*0.016,
-          :y => pl[:position][:y] + pl[:velocity][:y]*0.016}
-        e[:enemy][:action] = dist_to_player < sq(e[:enemy][:attack_radius]) ? :attack : :hunt
-      elsif e[:enemy][:target].nil? || dist_sq(e[:position][:x],e[:position][:y],e[:enemy][:target][:x],e[:enemy][:target][:y]) < sq(5)
-        e[:enemy][:target] = {:x => e[:position][:x] + Gosu::random(-50,50),
-          :y => e[:position][:y] + Gosu::random(-50,50)}
-        e[:enemy][:action] = :move
+      unless (pl = @engine.get_entity @player_id).nil?
+        dist_to_player = dist_sq(e[:position][:x],e[:position][:y],pl[:position][:x],pl[:position][:y])
+        if dist_to_player < sq(e[:enemy][:alert_radius])
+          e[:enemy][:target] = {:x => pl[:position][:x] + pl[:velocity][:x]*0.016,
+            :y => pl[:position][:y] + pl[:velocity][:y]*0.016}
+          e[:enemy][:action] = dist_to_player < sq(e[:enemy][:attack_radius]) ? :attack : :hunt
+        elsif e[:enemy][:target].nil? || dist_sq(e[:position][:x],e[:position][:y],e[:enemy][:target][:x],e[:enemy][:target][:y]) < sq(5)
+          e[:enemy][:target] = {:x => e[:position][:x] + Gosu::random(-50,50),
+            :y => e[:position][:y] + Gosu::random(-50,50)}
+          e[:enemy][:action] = :move
+        end
       end
       e
     end
@@ -128,14 +136,15 @@ class UniverseState < IngameState
       rad = Math::atan2(e[:velocity][:y], e[:velocity][:x])
       e[:rotation][:theta] = (rad/Math::PI)*180.0
 
-      if e[:enemy][:action] == :attack
+      if e[:enemy][:action] == :attack && (t-e[:weapon][:last_fire] > 1.0/e[:weapon][:fire_rate])
+        e[:weapon][:last_fire] = t
         @engine.add_entity(motion_components.merge({
           :position => e[:position].dup,
           :sprite => make_sprite(Gosu::Image.new @window, @spr_bullet),
           :rotation => {:theta => e[:rotation][:theta]},
-          :velocity => {x: @bullet_speed*Math.cos(rad) + e[:velocity][:x],
-            :y => @bullet_speed*Math::sin(rad) + e[:velocity][:y] },
-          :bullet => {:damage => @gun_damage, :owner => e[:id]},
+          :velocity => {x: e[:weapon][:speed]*Math.cos(rad) + e[:velocity][:x],
+            :y => e[:weapon][:speed]*Math::sin(rad) + e[:velocity][:y] },
+          :bullet => {:damage => e[:weapon][:damage], :owner => e[:id]},
           :life => 4, :lifetime => 4,
           :colour => 0xFFFF0000
         }))
@@ -179,7 +188,9 @@ class UniverseState < IngameState
         :hawking => 0.0,
         :health => 1.0,
         :probe => {:hawking_cap => @initial_hawking_cap, :xp => 0,
-          :health_cap => 1.0, :armour_mult => 1.0, :speed_mult => 1.0}
+          :health_cap => 1.0, :armour_mult => 1.0, :speed_mult => 1.0},
+        :weapon => {:fire_rate => @player_fire_rate, :last_fire => 0,
+          :damage => @player_damage, :speed => @player_bullet_speed}
       }))
 
     @engine.each_entity([:player]) { |e| @player_id = e[:id] }
@@ -233,14 +244,19 @@ class UniverseState < IngameState
     end
 
     5.times do
+      ex = Gosu::random(x1,x2)
+      ey = Gosu::random(y1, y2)
       @engine.add_entity({
-        :position => {:x => Gosu::random(x1,x2), :y => Gosu::random(y1, y2)},
-        :enemy => {:alert_radius => 300, :attack_radius => 100},
+        :position => {:x => ex, :y => ey},
+        :enemy => {:alert_radius => 300, :attack_radius => 100,
+          :target => {:x => ex, :y => ey}},
         :sprite => make_sprite(Gosu::Image.new @window, "spr_player.png"),
         :colour => 0xFFFF0000,
         :rotation => {:theta => Gosu::random(0,360)},
         :collidable => {:radius => 12},
-        :health => 0.5
+        :health => 0.5,
+        :weapon => {:fire_rate => @enemy_fire_rate, :last_fire => 0,
+          :damage => @enemy_damage, :speed => @enemy_bullet_speed}
       }.merge(motion_components))
     end
   end
